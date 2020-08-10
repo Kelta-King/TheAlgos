b289145529880fb8a8909aeb58ee0602e9340cfc
Kushang Shah <kushangshah131@gmail.com>
2020-08-10 08:31:17 +0530

    Algo explained

--- Math computations/Newton's_method.java
+++ Math computations/Newton's_method.java
@@ -1,10 +1,22 @@
+
+/*
+
+Here is the explanation of the code...
+there are 2 things required. Input number and tolerance value.
+
+In newton's method, if we want to find the square root of a number n then we can find the square root of that number bt 0.5(x + (n/x))
+Here x is assumed square root and root is proper square root.
+
+Now in a while loop find the root and if their difference is less than or equal to the the tolerance then that root will be answer. 
+Else that root will be new assumed root.
+
+*/
+
 import java.util.Scanner;
 
 class KeltaMath{
     
     static double square_root(double n){
-        
-        //Here I will use newton's method to calculate square root
         
         double x = n;
         double l = 0.000001; //tolerance
